26b4ca2d5afa97e9f0daf86f38ba30d1a5dff7a8
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-11-03 15:23:08 +0100

    chromium starts

--- cli.js
+++ cli.js
@@ -7,11 +7,15 @@ var wasm_buf = require('fs').readFileSync(process.argv[2])
 
 // TODO: check magic number for wasm
 
+// TODO: serve wasm from a localhost server that sets content-type to application/wasm
+
+// TODO: refactor, dont pass wasm_buf, it will be fetched, rename to genClientCode
 var js = packWebAssembly(wasm_buf)
-// sa this js
-return console.log(js)
 
-tapePuppetStream({ devtools: true }).on('data', console.log).end(js)
-// tapePuppetStream({ devtools: true }).end('console.log("fraud")')
+tapePuppetStream({ devtools: true })
+  .on('data', function (chunk) {
+    console.log(chunk.toString())
+  })
+  .end(js)
 
-// incorporate cli conventions
+// TODO: allow passing imports, incorporate cli conventions
--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 module.exports = function packWebAssembly (wasm_buf) {
   var base64_wasm = wasm_buf.toString('base64')
-  // BUG: chromium dies during startup
   return `
-    window.onload = function () {
+    (function () {
       function toUint8Array (s) {
         return new Uint8Array(atob(s).split('').map(charCode))
       }
-
       function charCode (c) {
         return c.charCodeAt(0)
       }
-
       function loadWebAssembly (base64_wasm, imp) {
         return new Promise(function (resolve, reject) {
           var wasm = toUint8Array(base64_wasm)
@@ -33,23 +30,35 @@ module.exports = function packWebAssembly (wasm_buf) {
             .catch(reject)
         })
       }
-
+      // TODO: fetch from a localhost server that sets content-type to application/wasm
       loadWebAssembly('${base64_wasm}')
         .then(function (mod) {
-          var info = document.createElement('p')
+          var ol = document.createElement('ol')
+          var li1 = document.createElement('li')
+          var li2 = document.createElement('li')
+          var li3 = document.createElement('li')
           var ta = document.createElement('textarea')
           var btn = document.createElement('button')
-          info.innerText = 'set breakpoints in yo wasm then run it by calling its exports in the textarea\'s function definition and clicking "run"'
-          ta.placeholder = 'fill this with a function definition that has this signature:\nfunction (module) // with module.exports.exported_func'
+          ol.appendChild(li1)
+          ol.appendChild(li2)
+          ol.appendChild(li3)
+          li1.innerText = 'set breakpoints in yo wasm'
+          li2.innerText = 'define a function in the textarea below'
+          li3.innerText = 'click run'
+          ta.placeholder = 'fill this with a function definition that has this signature: function (module) // with module.exports.exported_func'
+          ta.style.width = '50%'
+          ta.style.height = '100px'
           btn.innerText = 'run'
+          btn.style.display = 'block'
           btn.onclick = function () {
-            new Function('return ' + ta.innerText)()(mod)
+            // BUG: throws something
+            //new Function('return ' + ta.innerText)()(mod)
           }
-          document.body.appendChild(info)
+          document.body.appendChild(ol)
           document.body.appendChild(ta)
           document.body.appendChild(btn)
         })
         .catch(console.error)
-    }
+    })()
     `.replace(/^ {4}/gm, '')
 }
